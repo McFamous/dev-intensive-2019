b66770d7d6f779f2dee13c0a0b780635f6cc4bfb
McFamous <mcfamous100@gmail.com>
2020-03-10 20:18:26 +0300

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -0,0 +1,55 @@
+package ru.skillbranch.devintensive.extensions
+
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+
+fun Activity.hideKeyboard(){
+    val imm : InputMethodManager = applicationContext.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+}
+fun Activity.isKeyboardOpen(rootView : View) : Boolean{
+
+    /* 128dp = 32dp * 4, minimum button height 32dp and generic 4 rows soft keyboard */
+    val SOFT_KEYBOARD_HEIGHT_DP_THRESHOLD = 128;
+
+    val r = Rect()
+    rootView.getWindowVisibleDisplayFrame(r)
+    val dm: DisplayMetrics = rootView.resources.displayMetrics
+    /* heightDiff = rootView height - status bar height (r.top) - visible frame height (r.bottom - r.top) */
+    /* heightDiff = rootView height - status bar height (r.top) - visible frame height (r.bottom - r.top) */
+    val heightDiff: Int = rootView.bottom - r.bottom
+    /* Threshold size: dp to pixels, multiply with display density */
+    /* Threshold size: dp to pixels, multiply with display density */
+    val isKeyboardShown: Boolean = heightDiff > SOFT_KEYBOARD_HEIGHT_DP_THRESHOLD * dm.density
+
+    if(isKeyboardShown)
+        Log.d("M_Activity","Keyboard open")
+    else
+        Log.d("M_Activity","Keyboard close")
+
+    return isKeyboardShown
+}
+fun Activity.isKeyboardClosed(rootView : View) : Boolean{
+    /* 128dp = 32dp * 4, minimum button height 32dp and generic 4 rows soft keyboard */
+    val SOFT_KEYBOARD_HEIGHT_DP_THRESHOLD = 128;
+
+    val r = Rect()
+    rootView.getWindowVisibleDisplayFrame(r)
+    val dm: DisplayMetrics = rootView.resources.displayMetrics
+    /* heightDiff = rootView height - status bar height (r.top) - visible frame height (r.bottom - r.top) */
+    /* heightDiff = rootView height - status bar height (r.top) - visible frame height (r.bottom - r.top) */
+    val heightDiff: Int = rootView.bottom - r.bottom
+    /* Threshold size: dp to pixels, multiply with display density */
+    /* Threshold size: dp to pixels, multiply with display density */
+    val isKeyboardShown: Boolean = heightDiff > SOFT_KEYBOARD_HEIGHT_DP_THRESHOLD * dm.density
+
+    Log.d("M_Activity","Keyboard open")
+
+    return isKeyboardShown
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,104 @@
+package ru.skillbranch.devintensive.models
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+
+class Bender(
+    var status:Status = Status.NORMAL,
+    var question:Question = Question.NAME) {
+
+    fun askQuestion() : String = when(question){
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+    fun listenAnswer(answer : String) : Pair<String, Triple<Int, Int, Int>>{
+        val pattern : Pattern = Pattern.compile("[0-9]")
+        val matcher : Matcher = pattern.matcher(answer)
+        return if(question.answers.contains(answer)){
+            question = question.nextQuestion()
+            "Отлично - ты справился\n${question.question}" to status.color
+        }
+        else if(testOnLowerCase(answer) &&  question == Question.NAME){
+            "${question.correctEnter()}\n${question.question}" to status.color
+        }
+        else if(!testOnLowerCase(answer) &&  question == Question.PROFESSION){//
+            "${question.correctEnter()}\n${question.question}" to status.color
+        }
+        else if(!matcher.find() && question == Question.MATERIAL ){//
+            "${question.correctEnter()}\n${question.question}" to status.color
+        }
+        else if(matcher.find() && question == Question.BDAY ){
+            "${question.correctEnter()}\n${question.question}" to status.color
+        }
+        else if(matcher.find() && question == Question.MATERIAL || answer.length != 7 ){
+            "${question.correctEnter()}\n${question.question}" to status.color
+        }
+        else{
+            status = status.nextStatus()
+            if(status == Status.NORMAL && question != Question.IDLE){
+                question = Question.NAME
+                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+            }
+            else
+                "Это неправильный ответ\n${question.question}" to status.color
+        }
+    }
+
+    enum class Status(val color : Triple<Int,Int,Int>){
+        NORMAL(Triple(255,255,255)),
+        WARNING(Triple(255,255,0)),
+        DANGER(Triple(255,120,0)),
+        CRITICAL(Triple(255,60,60));
+
+        fun nextStatus() : Status{
+            return if(this.ordinal < values().lastIndex){
+                values()[this.ordinal + 1]
+            }else{
+                values()[0]
+            }
+        }
+    }
+    enum class Question(val question:String, val answers:List<String>){
+
+        NAME("Как меня зовут?", listOf("Бендер","Bender")) {
+            override fun nextQuestion(): Question = PROFESSION
+            override fun correctEnter(): String = "Имя должно начинаться с заглавной буквы"
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик","bender")) {
+            override fun nextQuestion(): Question = MATERIAL
+            override fun correctEnter(): String = "Профессия должна начинаться со строчной буквы"
+        },
+        MATERIAL("Из чего я сделан?", listOf("дерево","металл", "metal","iron","wood")) {
+            override fun nextQuestion(): Question = BDAY
+            override fun correctEnter(): String = "Материал не должен содержать цифр"
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun nextQuestion(): Question = SERIAL
+            override fun correctEnter(): String = "Год моего рождения должен содержать только цифры"
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override fun nextQuestion(): Question = IDLE
+            override fun correctEnter(): String = "Серийный номер содержит только цифры, и их 7"
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun nextQuestion(): Question = IDLE
+            override fun correctEnter(): String = ""
+        };
+
+        abstract fun nextQuestion() : Question
+        abstract fun correctEnter() : String
+    }
+    private fun isDigit(str : String) : Boolean{
+        return str.toIntOrNull() != null
+    }
+
+    fun testOnLowerCase(str : String) : Boolean{
+        val lowerStr = str.toLowerCase()
+        return str == lowerStr
+    }
+}
