eea6b9173bdf4426100d6b3b399bd266fa17d771
McFamous <mcfamous100@gmail.com>
2020-03-11 20:41:40 +0300

    hometask_3 last_try

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -55,10 +55,10 @@ class Bender(
     }
 
     enum class Status(val color : Triple<Int,Int,Int>){
-        NORMAL(Triple(255,255,255)),
-        WARNING(Triple(255,255,0)),
-        DANGER(Triple(255,120,0)),
-        CRITICAL(Triple(255,60,60));
+        NORMAL(Triple(255, 255, 255)) ,
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0)) ;
 
         fun nextStatus() : Status{
             return if(this.ordinal < values().lastIndex){
