c4103e2002cdacaf5aebe14cfac67e22cc2c324b
McFamous <mcfamous100@gmail.com>
2020-03-28 14:49:26 +0300

    hometask_4

--- app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/custom/CircleImageView.kt
@@ -1,9 +1,12 @@
 package ru.skillbranch.devintensive.ui.custom
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
 import androidx.annotation.ColorRes
 import androidx.annotation.Dimension
+import androidx.core.graphics.toColor
 import ru.skillbranch.devintensive.R
 
 
@@ -13,19 +16,36 @@ class CircleImageView @JvmOverloads constructor(
     defStyleAttr : Int = 0
 ): androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyleAttr) {
 
-//    @Dimension
-//    fun getBorderWidth() : Int = resources.getDimension(R.dimen.cv_borderWidth).toInt()
-//
-//    fun setBorderWidth(@Dimension dp : Int){
-//    }
-//
-//    fun getBorderColor() : Int = R.color.cv_borderColor
-//
-//    fun setBorderColor(hex : String){
-//
-//    }
-//
-//    fun setBorderColor(@ColorRes colorId : Int){
-//
-//    }
+    private var borderWidth = getBorderWidth()
+    private var borderColor = getBorderColor()
+
+    @Dimension
+    fun getBorderWidth():Int = resources.getDimension(R.dimen.cv_borderWidth).toInt()
+
+    fun setBorderWidth(@Dimension dp:Int){
+        if(getBorderWidth() == dp)
+            return
+        borderWidth = dp
+    }
+
+    private fun getBorderColor():Int = resources.getColor(R.color.cv_borderColor)
+
+    fun setBorderColor(hex:String){
+        getBorderColor()
+    }
+
+    fun setBorderColor(@ColorRes colorId: Int){
+        if(getBorderColor() == colorId)
+            return
+        borderColor = colorId
+    }
+    override fun onDraw(canvas: Canvas?) {
+        var backgroundPaint : Paint = Paint()
+        backgroundPaint.color = borderColor
+        canvas?.drawCircle(width/2.toFloat(),
+            height/2.toFloat(),
+            R.dimen.avatar_round_size.toFloat(),
+            backgroundPaint)
+
+    }
 }
--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -5,6 +5,8 @@ import android.graphics.ColorFilter
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.KeyEvent
 import android.view.View
@@ -35,6 +37,7 @@ class ProfileActivity : AppCompatActivity() {
 
     private lateinit var viewModel: ProfileViewModel
     var isEditMode = false
+    var valid = true
     lateinit var viewFields : Map<String,TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -95,6 +98,66 @@ class ProfileActivity : AppCompatActivity() {
         btn_switch_theme.setOnClickListener {
             viewModel.switchTheme()
         }
+        //!!!!!
+//        iv_avatar.alpha = 1F
+//        iv_avatar.setBorderColor(R.color.design_default_color_primary)
+
+        et_repository.addTextChangedListener(object : TextWatcher{//!!!!!
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                if(s.toString() == "")
+                    valid = true
+                else{
+                    var slash = 0
+                    for(i in 1..s.toString().length){
+                        if(s.toString()[i - 1] == '/')
+                            slash++
+                    }
+                    if(s.toString().contains("https://")){
+                        valid = when {
+                            s.toString().contains("https://github.com/") && slash == 3 -> true
+                            s.toString().contains("https://www.github.com/") && slash == 3 -> true
+                            else -> false
+                        }
+
+                    }
+                    else if(s.toString()[0] == 'w' && s.toString().contains("www") && slash == 1){
+                        if(s.toString().contains("www.github.com/"))
+                            valid = true
+                    }
+                    else valid = s.toString()[0] == 'g' && s.toString().contains("github.com/") && slash == 1
+
+                    if(valid){
+                        valid = s.toString()[s.toString().length - 1] != '/'
+                        valid = !s.toString().endsWith("enterprise")
+                        valid = !s.toString().endsWith("features")
+                        valid = !s.toString().endsWith("topics")
+                        valid = !s.toString().endsWith("collections")
+                        valid = !s.toString().endsWith("trending")
+                        valid = !s.toString().endsWith("events")
+                        valid = !s.toString().endsWith("marketplace")
+                        valid = !s.toString().endsWith("pricing")
+                        valid = !s.toString().endsWith("nonprofit")
+                        valid = !s.toString().endsWith("customer-stories")
+                        valid = !s.toString().endsWith("security")
+                        valid = !s.toString().endsWith("login")
+                        valid = !s.toString().endsWith("join")
+                    }
+                }
+                if(!valid)
+                    wr_repository.error = "Невалидный адрес репозитория"
+                else
+                    wr_repository.error = ""
+            }
+        })
     }
 
     private fun showCurrentMode(isEdit: Boolean) {
@@ -141,6 +204,8 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun saveProfileInfo(){
+        if(!valid){
+            et_repository.text = null // try
         Profile(
             firstName = et_first_name.text.toString(),
             lastName = et_last_name.text.toString(),
@@ -152,3 +217,5 @@ class ProfileActivity : AppCompatActivity() {
     }
 
 }
+}
+
